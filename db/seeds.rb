d210912fb3411aebdc06d2736c9441b5658afbc3
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 16:18:27 -0600

    Add seeds using factory bot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include FactoryBot::Syntax::Methods
+
+Pet.destroy_all
+Shelter.destroy_all
+Application.destroy_all
+PetApp.destroy_all
+
+shelter1 = create(:shelter)
+shelter2 = create(:shelter)
+shelter3 = create(:shelter)
+
+pet1 = create(:pet, shelter: shelter1)
+pet2 = create(:pet, shelter: shelter1)
+pet3 = create(:pet, shelter: shelter2)
+pet4 = create(:pet, shelter: shelter2)
+pet5 = create(:pet, shelter: shelter2)
+pet6 = create(:pet, shelter: shelter3)
+
+app1 = create(:application)
+app2 = create(:application)
+app3 = create(:application)
+
+PetApp.create!(application: app1, pet: pet1)
+PetApp.create!(application: app1, pet: pet2)
+PetApp.create!(application: app1, pet: pet3)
+PetApp.create!(application: app2, pet: pet3)
+PetApp.create!(application: app2, pet: pet4)
+PetApp.create!(application: app3, pet: pet4)
+PetApp.create!(application: app3, pet: pet5)
+PetApp.create!(application: app3, pet: pet6)
