58c17388f75a1edf94565195cbd2565307d97172
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 08:52:14 -0600

    Add factory bot and faker gems

---
+++ spec/factories/application.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :application, class: Application do
+    name { Faker::TvShows::RickAndMorty.unique.character }
+    street_address  { Faker::Address.unique.street_address }
+    city { Faker::Address.city  }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip_code }
+    description { Faker::TvShows::RickAndMorty.unique.quote }
+    # pets { Faker::TvShows::SouthPark.unique.character }
+    status { ["In Progress", "Pending", "Accepted", "Rejected"].sample }
+  end
+end
