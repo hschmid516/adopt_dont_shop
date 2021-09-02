8135e3b3da57995390ec168e3dd7c3016e1139c8
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 16:51:12 -0600

    Add timestamps to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_213423) do
+ActiveRecord::Schema.define(version: 2021_09_02_224719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_09_02_213423) do
     t.string "zip"
     t.string "description"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pet_apps", force: :cascade do |t|
