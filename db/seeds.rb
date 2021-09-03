9f36883aa551f66e6d79cce9ed479aeda56492b1
Henry Schmid <henry.schmid1@gmail.com>
2021-09-03 14:33:19 -0600

    Add require factory bot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
 include FactoryBot::Syntax::Methods
 
 Pet.destroy_all
