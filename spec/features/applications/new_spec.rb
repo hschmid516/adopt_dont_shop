5126d7561fff450692be1cfdf33069f181c18de1
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 23:44:18 -0600

    Add test for messages if fields not filled

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -19,7 +19,54 @@ RSpec.describe 'new application page' do
     expect(page).to have_content(Application.last.city)
     expect(page).to have_content(Application.last.state)
     expect(page).to have_content(Application.last.zip)
-    expect(page).to have_content(Application.last.description)
+    expect(page).to have_content("Description:")
     expect(page).to have_content("Status: In Progress")
+  end
+
+  it 'shows error message if fields not complete' do
+    fill_in 'Street address', with: '12 Grimmauld Place'
+    fill_in 'City', with: 'Littleton'
+    fill_in 'State', with: 'CO'
+    fill_in 'Zip', with: '80127'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Name can't be blank")
+
+    fill_in 'Name', with: 'Henry Schmid'
+    fill_in 'City', with: 'Littleton'
+    fill_in 'State', with: 'CO'
+    fill_in 'Zip', with: '80127'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Street address can't be blank")
+
+    fill_in 'Name', with: 'Henry Schmid'
+    fill_in 'Street address', with: '12 Grimmauld Place'
+    fill_in 'State', with: 'CO'
+    fill_in 'Zip', with: '80127'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("City can't be blank")
+
+    fill_in 'Name', with: 'Henry Schmid'
+    fill_in 'Street address', with: '12 Grimmauld Place'
+    fill_in 'City', with: 'Littleton'
+    fill_in 'Zip', with: '80127'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("State can't be blank")
+
+    fill_in 'Name', with: 'Henry Schmid'
+    fill_in 'Street address', with: '12 Grimmauld Place'
+    fill_in 'City', with: 'Littleton'
+    fill_in 'State', with: 'CO'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Zip can't be blank")
   end
 end
