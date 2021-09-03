7d7e5b601377f5bb9643124cea1d226389102f09
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 23:44:36 -0600

    Add validates_presence_of for applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
+  validates_presence_of :name,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zip
+
   has_many :pet_apps, dependent: :delete_all
   has_many :pets, through: :pet_apps
 end
