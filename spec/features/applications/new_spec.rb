bfd3b304c695fb8ea9b3f4988bf739cfbd0d344a
Henry Schmid <henry.schmid1@gmail.com>
2021-09-02 20:46:35 -0600

    Add test for new app info on show page

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -12,7 +12,14 @@ RSpec.describe 'new application page' do
     fill_in 'State', with: 'CO'
     fill_in 'Zip', with: '80127'
     click_button 'Submit'
-    
+
     expect(current_path).to eq("/applications/#{Application.last.id}")
+    expect(page).to have_content(Application.last.name)
+    expect(page).to have_content(Application.last.street_address)
+    expect(page).to have_content(Application.last.city)
+    expect(page).to have_content(Application.last.state)
+    expect(page).to have_content(Application.last.zip)
+    expect(page).to have_content(Application.last.description)
+    expect(page).to have_content("Status: In Progress")
   end
 end
